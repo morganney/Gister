11522b2941493c3b0d6e5fc6223b13dd832979ba
Morgan Ney <morganney@gmail.com>
2014-06-14 20:40:16 -0700

    added tests for 'poll' and 'observe'

--- test/js/gister.spec.js
+++ test/js/gister.spec.js
@@ -11,6 +11,8 @@ require(['gister'], function(Gister) {
 
   describe('Gister', function() {
 
+    this.timeout(5000);
+
     it('Should be defined globally when used with an inline <script> tag', function() {
       window.Gister.should.be.an.instanceof(Object).and.should.not.be.exactly(Gister);
     });
@@ -36,23 +38,26 @@ require(['gister'], function(Gister) {
 
         it('Should require a String as the first argument', function() {
           (function() {
-            var gister = new Gister();
+            new Gister();
           }).should.throw(/provide\ a\ data\ attr\.\ name/i);
+        });
+
+        it('Should accept a Function as the second argument', function() {
+          (function() {
+            new Gister('test', function(el) {/* 'el' is the <code> node that was just updated */ });
+          }).should.not.throw();
         });
 
       });
 
       describe('#fetch', function() {
 
-        beforeEach(function() {
-          $('body').append("<code data-gist-id='11103140' id='targeted'></code>");
-        });
-
         afterEach(function() {
-          $('#targeted').remove();
+          $('#fixtures').empty();
         });
 
         it('Should update targeted <code> elements already present in the DOM', function(done) {
+          $('#fixtures').append("<code data-gist-id='11103140' id='targeted'></code>");
           new Gister('gist-id', function(el) {
             el.className.should.match(/gisterComplete/);
             el.innerHTML.should.not.be.empty;
@@ -60,14 +65,100 @@ require(['gister'], function(Gister) {
           }).fetch();
         });
 
-        it('Should execute any callback function passed to Gister constructor', function(done) {
+        it('Should execute any callback function passed to Gister Constructor', function(done) {
+          $('#fixtures').append("<code data-gist-id='11103140' id='targeted'></code>");
           new Gister('gist-id', function() { done() }).fetch();
         });
       });
-    } else {
-      it('Should not be tested further until ES6 Promise dependency met', function() {
-        true;
+
+      describe('#observe', function() {
+
+        beforeEach(function() {
+          this.clock = sinon.useFakeTimers();
+        });
+
+        afterEach(function() {
+          $('#fixtures').empty();
+          this.clock.restore();
+        });
+
+        it('Should update any targeted, children <code> elements of the observed DOM node after it undergoes a mutation', function(done) {
+          $('#fixtures').append("<div id='observed'></div>");
+          new Gister('gist-id', function(el) {
+            el.className.should.match(/gisterComplete/);
+            el.innerHTML.should.not.be.empty;
+            done();
+          }).observe('#observed');
+          setTimeout(function() { $('#observed').append("<code data-gist-id='11106519' id='targeted'></code>") }, 2000);
+          this.clock.tick(2000);
+        });
+
+        it('Should execute any callback function passed to Gister Constructor', function(done) {
+          $('#fixtures').append("<div id='observed'></div>");
+          new Gister('gist-id', function() { done() }).observe('#observed');
+          setTimeout(function() { $('#observed').append("<code data-gist-id='11106519' id='targeted'></code>") }, 2000);
+          this.clock.tick(2000);
+        });
       });
+
+      describe('#poll', function() {
+
+        beforeEach(function() {
+          this.clock = sinon.useFakeTimers();
+        });
+
+        afterEach(function() {
+          //$('#fixtures').empty();
+          this.clock.restore();
+        });
+
+        it('Should update any targeted, children <code> elements of the polled DOM node after it undergoes a mutation', function(done) {
+          $('#fixtures').append("<div id='polled'></div>");
+          new Gister('gist-id', function(el) {
+            el.className.should.match(/gisterComplete/);
+            el.innerHTML.should.not.be.empty;
+            done();
+            $('#fixtures').empty();
+          }).poll('#polled');
+          setTimeout(function() { $('#polled').append("<code data-gist-id='11120666' id='targeted'></code>") }, 2000);
+          this.clock.tick(2000);
+        });
+
+        it('Should execute any callback function passed to Gister Constructor', function(done) {
+          $('#fixtures').append("<div id='polled'></div>");
+          new Gister('gist-id', function() { done(); $('#fixtures').empty(); }).poll('#polled');
+          setTimeout(function() { $('#polled').append("<code data-gist-id='11120666' id='targeted'></code>") }, 2000);
+          this.clock.tick(2000);
+        });
+
+        it('Should not poll the DOM node for more than 10 seconds', function(done) {
+          var targeted = undefined
+            , cb       = sinon.spy()
+            , me       = this
+          ;
+
+          $('#fixtures').append("<div id='polled'></div>");
+          new Gister('gist-id', cb).poll('#polled');
+
+          // Wait 10 (fake) seconds before upating polled DOM node
+          setTimeout(function() {
+            $('#polled').append("<code data-gist-id='11120666' id='targeted'></code>");
+            // Now wait 5 (fake) seconds for any network activity to occur
+            setTimeout(function() {
+              targeted = document.querySelector('code#targeted');
+              console.dir(targeted); // TODO: if outer timeout set to 1s, tests still pass! Why?!
+              cb.callCount.should.be.exactly(0);
+              targeted.className.should.not.match(/gisterComplete/);
+              targeted.innerHTML.should.be.empty;
+              done();
+            }, 5000);
+            me.clock.tick(5000);
+          }, 10000);
+          this.clock.tick(10000);
+        });
+      });
+    } else {
+      it('Should undergo limited testing when ES6 Promise dependency is NOT met', function() { true });
 
       describe('#Constructor', function() {
         it('Should require ES6 Promises to be available', function() {
@@ -79,8 +170,6 @@ require(['gister'], function(Gister) {
     }
   });
 
-  mocha.setup({
-    timeout: 3000,
-    ignoreLeaks: true // Gister creates global JSONP callbacks
-  }).run();
+  // Start mocha ignoring globals (Gister uses dynamic globals for JSONP callbacks)
+  mocha.setup({ ignoreLeaks: true }).run();
 });
