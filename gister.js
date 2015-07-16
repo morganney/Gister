9262b32e281a83faf8d2889df4d5054d975bbdfc
Morgan Ney <morgan@localhost.localdomain>
2015-07-15 22:08:18 -0700

    add support for commonjs, some refactoring.

--- gister.js
+++ gister.js
@@ -1,139 +1,111 @@
 /**
- * Gister provides dynamic embedding of GitHub Gists using MutationObservers and Promises.
- * It can be loaded as an AMD module or inline script element.
- *
- * Gister embeds gist content into HTML5 code elements with a data-* attribute:
- * <code data-publicGistId='12345'>
- *
- * Gister also works without MutationObservers on static content if loaded via an inline
- * script tag using a data-attrName attribute:
- * <script data-attrName='foobar' src='gister.js'></script> (updates) <code data-foobar='[public gist id]'></code>
- *
- * WARNING: This script isn't cross-browser in any way. In particular, it doesn't
- * cater to Internet Explorer's inabilities, and only supports the latest versions
- * of modern browsers that implement the MutationObserver and Promise API's.
- *
- *********************************************************************************************
- *  This basically means that only Chrome 32+ supports Gister by default (as of 1/28/2014).  *
- *********************************************************************************************
- *
- * By adding the suggested Promise polyfill, the script can support
- * Chrome 18+, Firefox 14+, Safari 6+, Opera 15+, and IE 11+.
- *
- * If you want better browser support, in particular for IE, or you want Gister to
- * be more configurable, fork me.
- *
- * For mobile browser support @see http://caniuse.com/
- *
- * Recommended Promises polyfill: @see https://github.com/jakearchibald/ES6-Promises
- */
+* Gister provides dynamic embedding of GitHub Gists using MutationObservers and
+* Promises. It can be loaded as a module (CommonJS/AMD) or <script>.
+*
+* Gister embeds gist content into HTML5 code elements with a data-* attribute:
+* <code data-publicGistId='12345'>
+*
+* Gister also works without MutationObservers on static (server-generated)
+* content if loaded as a <script> with a set data-attrName property:
+* e.g. <script data-attrName='gist' src='gister.js'></script> will update
+* <code data-gist='[public gist id]'></code> elements on page load.
+*
+* WARNING: Gister is only supported by modern browsers that implement Promises.
+*
+*/
 (function(root, modef, undefined) {
-
   var inlineScript = undefined;
 
-  if(typeof define === 'function' && define.amd) {
-    define(modef); // AMD
-  } else {
-    root.Gister = modef(); // <script>
+  // CommonJs
+  if(typeof module !== 'undefined' && module.exports) module.exports = modef();
+  // AMD
+  else if(typeof define === 'function' && define.amd) define(modef);
+  // <script>
+  else {
+    root.Gister = modef();
 
-    // If <script data-attrName> used, bypass MutationObserver API and update DOM immediately.
-    inlineScript = document.querySelector('script[src*="gister"]');
+    // update inline scripts with data-attrName immediately
+    inlineScript = document.querySelector('script[src*="gister"');
     if(inlineScript && inlineScript.dataset.attrName) {
       new root.Gister(inlineScript.dataset.attrName).fetch();
     }
   }
-
 }(this, function() {
   /**
-   * Constructor for Gister. The dataAttrName parameter is required as the data attribute name value
-   * of the DOM node where the gist is to be embedded.
+   * Constructor for Gister.
    * @example
-   * 	If gist's parent node is <code data-gistId='12345'> then dataAttrName === 'gistId'
-   *	If gist's parent node is <code data-foo-bar='12345'> then dataAttrName === 'foo-bar'
+   * 	For gists embedded into:
+   *    <code data-gistId='12345'> then dataAttrName === 'gistId'
+   *    <code data-foo-bar='12345'> then dataAttrName === 'foo-bar'
    *
-   * @param {String:dataAttrName(req)} The name of the gist's parent node's data attribute. Required.
-   * @param {Function:callback} Optional callback to be called after each gist is embedded.
+   * @param {String:dataAttrName(req)} Required
+   * @param {Function:callback} Optional callback invoked after embedding gist
    */
   var	Gister = function(dataAttrName, callback) {
 
-    // Check for Promise support. Gister requires ES6 Promises.
-    if(!window.Promise) {
-      throw new Error("Failed to construct 'Gister': 'Promise' is not defined.");
-    }
-
-    // Enforce constructor invocation.
-    if(!(this instanceof Gister)) {
-      throw new TypeError("Failed to construct 'Gister': Use the 'new' operator.");
-    }
-
-    // Enforce parameter requirement.
-    if(!dataAttrName) {
-      throw new Error("Failed to construct 'Gister': Provide a data attr. name for the gist container(s).");
-    }
-
     // Private properties and/or methods.
-    var w = window, d = document, me = this,
-      attrName = 'data-' + dataAttrName,
-      gistSelector = ['code[', attrName, ']'].join(''),
-      addGistCss = function(filename) {
-        var link = d.createElement('link'),
-          fn   = filename.indexOf('http') > -1 ? filename : 'https://gist-assets.github.com' + filename;
+    var w = window;
+    var d = document;
+    var me = this;
+    var gistSelector = ['code[data-', dataAttrName, ']'].join('');
+    var dataSetName = dataAttrName.replace(/-([a-z])/g, function(g) {return g[1].toUpperCase();});
+    var addGistCss = function(filename) {
+      var link = d.createElement('link');
+      var fn = filename.indexOf('http') > -1 ? filename : 'https://gist-assets.github.com' + filename;
 
-        link.type = 'text/css';
-        link.rel = 'stylesheet';
-        link.media = 'all';
-        link.href = fn;
+      link.type = 'text/css';
+      link.rel = 'stylesheet';
+      link.media = 'all';
+      link.href = fn;
 
-        d.getElementsByTagName('head')[0].appendChild(link);
-        // Only need to append the CSS stylesheet once.
-        addGistCss = function() {};
-      },
-      mutationCallback = function() {
-        // Convert NodeList into an Array of Nodes. Older versions of IE throw and error here.
-        Array.prototype.slice.call(d.querySelectorAll(gistSelector)).forEach(function(el) {
-          fetch(el.getAttribute(attrName)).then(function(gist) {
-            addGistCss(gist.stylesheet);
-            el.innerHTML = gist.div;
-            el.className = el.className + ' gisterComplete';
-          }).catch(function(error) {
-            el.className = el.className + ' gisterError';
-            el.innerHTML = error.message;
-          }).then(function() {
-            // Always execute a user supplied callback. Usually for adding/removing CSS styles.
-            if(callback && typeof callback === 'function') callback(el);
-          });
+      d.getElementsByTagName('head')[0].appendChild(link);
+      // Only need to append the CSS stylesheet once.
+      addGistCss = function() {};
+    };
+    var mutationCallback = function(mutations) {
+      var gistNodes = Array.prototype.slice.call(d.querySelectorAll(gistSelector));
+
+      gistNodes.forEach(function(el) {
+        fetch(el.dataset[dataSetName]).then(function(gist) {
+          addGistCss(gist.stylesheet);
+          el.innerHTML = gist.div;
+          el.className = el.className + ' gisterComplete';
+        }).catch(function(error) {
+          el.className = el.className + ' gisterError';
+          el.innerHTML = error.message;
+        }).then(function() {
+          if(callback && typeof callback === 'function') callback(el);
         });
-        // Just listen once.
-        observer.disconnect();
-      },
-      observer = w.MutationObserver ? new MutationObserver(mutationCallback) : undefined;
+      });
+
+      // Just listen once.
+      observer.disconnect();
+    };
+    var observer = w.MutationObserver ? new MutationObserver(mutationCallback) : undefined;
 
     /**
-     * Private function for fetching a GitHub gist. Uses JSONP because gists.github.com
-     * doesn't support CORS (api.github.com supports CORS but is rate-limited).
-     *
-     * Returns a Promise object.
+     * Private function for fetching a GitHub gist using JSONP.
      *
      * @param {String:id} The public gist id.
      * @returns {Object} A Promise of a gist.
      */
     function fetch(id) {
       return new Promise(function(resolve, reject) {
-        var s = d.createElement('script'),
-          first = d.getElementsByTagName('script')[0],
-          callbackName = '_f_gist' + id,
-          gistName = '_p_gist' + id,
-          overwritten = typeof w[callbackName] !== 'undefined' ? true : false,
-          preserved = w[callbackName],
-          cleanup = function() {
-            if(overwritten) w[callbackName] = preserved;
-            else delete w[callbackName];
-          };
+        var s = d.createElement('script');
+        var first = d.getElementsByTagName('script')[0];
+        var callbackName = '_f_gist' + id;
+        var gistName = '_p_gist' + id;
+        var overwritten = typeof w[callbackName] !== 'undefined' ? true : false;
+        var preserved = w[callbackName];
+        var cleanup = function() {
+          if(overwritten) w[callbackName] = preserved;
+          else delete w[callbackName];
+        };
 
         /**
-         * The inserted script's JSONP callback. Attach it to global scope before appending script.
+         * The inserted script's JSONP callback.
          *
-         * @param {JSON:gist} Response from https://gists.github.com/{gist id}.json.
+         * @param {JSON:gist}
          */
         w[callbackName] = function(gist) {
           w[gistName] = gist;
@@ -144,7 +116,7 @@
 
         /**
          * The inserted script's onload handler.
-         * Resolves the raw JSON gist from gists.github.com.
+         * Resolves the raw JSON gist.
          */
         s.onload = function() {
           resolve(w[gistName]);
@@ -161,20 +133,32 @@
         };
 
         /**
-         * Add a timeout in case GitHub is busy or some other unknown network issue is afoot.
+         * Add a timeout to defend against unkown network issues.
          * Rejects the promise if GitHub takes 10 seconds to respond.
          *
-         * Once a Promise is fulfilled or rejected, it will NEVER change its state again, so
-         * rejecting after some set time won't overwrite any previous fulfillment or rejection.
+         * Once a Promise is fulfilled or rejected its state won't change so
+         * rejecting after some set time won't overwrite any previous state.
          */
         setTimeout(function() {
           reject(new Error('The request to GitHub has timed out.'));
-          // Bypass cleanup() to avoid any runtime errors from a missing function reference.
+          // Bypass cleanup() to avoid any errors from a missing function reference.
         }, 10000);
 
         // Add the <script> to the DOM
         first.parentNode.insertBefore(s, first);
       });
+    }
+
+    if(!(this instanceof Gister)) {
+      throw new TypeError("Failed to construct 'Gister': Use the 'new' operator.");
+    }
+
+    if(!dataAttrName) {
+      throw new Error("Failed to construct 'Gister': Provide a data attr. name for the gist container(s).");
+    }
+
+    if(!w.Promise) {
+      throw new Error("Failed to construct 'Gister': Your browser doesn't support 'Promise'.");
     }
 
     // Protected properties and/or methods
@@ -183,9 +167,9 @@
      * Observes a DOM node for changes prior to embedding GitHub gists.
      * Uses MutationObserver's observe() method.
      *
-     * NOTE: The node identified by 'selector' must already be in the DOM prior to calling.
+     * NOTE: The node identified by 'selector' must be present in the DOM.
      *
-     * @param {String:selector} A Selectors API (Level 2) string identifying the gists parent node in the DOM.
+     * @param {String:selector} A Selectors API (Level 2) string
      */
     this.observe = function(selector) {
       if(!observer) throw new Error("Your browser doesn't support 'MutationObserver'.");
@@ -193,30 +177,26 @@
     };
 
     /**
-     * Polls the DOM node targed by 'selector' for gist parent nodes (containers).
-     * This is a fallback for browser's with Promise support but no MutationObserver support.
-     * Really only useful for slightly older versions of IE.
+     * Polls the DOM tree under 'selector' for gist containers for 10 seconds.
+     * This is a fallback for browser's without MutationObserver support.
      *
-     * Will only poll the DOM for 10 seconds, so call this method at around the time
-     * you expect the DOM to change.
-     *
-     * @param {String:selector} A Selectors API (Level 2) string identifying the gists parent node in the DOM.
+     * @param {String:selector} A Selectors API (Level 2) string
      */
     this.poll = function(selector) {
-      var node = document.querySelector(selector),
-        start = new Date().getTime(),
-        timer = undefined;
+      var node = document.querySelector(selector);
+      var start = new Date().getTime();
+      var timer = undefined;
 
       (function recurse() {
         var elapsed = (new Date().getTime()) - start;
 
-        if(elapsed >= 10 * 1000) { // Only poll the DOM for 10 seconds, no more.
+        if(elapsed >= 10 * 1000) {
           clearTimeout(timer);
         } else if(node.querySelectorAll(gistSelector).length) {
           clearTimeout(timer);
           mutationCallback(null);
         } else {
-          timer = setTimeout(recurse, 500); // Poll the DOM twice a second.
+          timer = setTimeout(recurse, 500);
         }
       }());
 
